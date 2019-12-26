6ad54b13013c356188f369f7cfb3521fb1bb6fad
Your Name <1234.kirill@list.ru>
2019-12-26 14:59:01 +0300

    Add customer model

--- customers_app/admin.py
+++ customers_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from customers_app.models import Customer
 # Register your models here.
+
+
+admin.site.register(Customer)
--- customers_app/models.py
+++ customers_app/models.py
@@ -1,3 +1,19 @@
+
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
+
+class Customer(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, blank=False)
+
+    first_name = models.CharField(max_length=64, blank=True)
+    last_name = models.CharField(max_length=64, blank=True)
+    age = models.PositiveSmallIntegerField(blank=True)
+
+    date_of_birth = models.DateField(blank=True)
+    photo = models.ImageField(blank=True)
+    
+    def __str__(self):
+        return "{0} {1}".format(self.last_name, self.first_name)
