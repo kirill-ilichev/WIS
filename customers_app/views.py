5d3206ed4f38cb0f41e167ea1768be5a415d480a
Your Name <1234.kirill@list.ru>
2019-12-26 15:58:35 +0300

    Add CustomersView

--- customers_app/views.py
+++ customers_app/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+from customers_app.models import Customer
+
+
+class CustomersView(TemplateView):
+    template_name = "customers.html"
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        
+        customers = Customer.objects.all()
+        query_filter = self.request.GET.get('filter', None)
+        if query_filter:
+            customers = customers.order_by('-{}'.format(query_filter))
+
+        context['customers'] = customers
+        return context
