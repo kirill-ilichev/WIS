757d753f06b9f15b1503c82ada96fe052158bdb8
kirill-ilichev <1234.kirill@list.ru>
2020-01-04 13:40:35 +0300

    Change url structure

--- WIS/urls.py
+++ WIS/urls.py
@@ -1,20 +1,12 @@
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from . import settings
-from customers_app.views import CustomersListView, CustomersCreateView, CustomersAuthView, CustomersDetailView,\
-                                export_customers_details_in_xlsx, CustomersVotingView, CustomersVotingAPIView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('customers/', CustomersListView.as_view(), name='customers-list'),
-    path('customers/auth/', CustomersAuthView.as_view(), name='customers-auth'),
-    path('customers/create/', CustomersCreateView.as_view(), name='customers-create'),
-    path('customers/<int:pk>/', CustomersDetailView.as_view(), name='customers-detail'),
-    path('customers/export/', export_customers_details_in_xlsx),
-    path('customers/voting/', CustomersVotingView.as_view(), name='customers-voting'),
-    path('api/customers/voting/', CustomersVotingAPIView.as_view(), name='api-customers-voting')
+    path('customers/', include('customers_app.urls')),
 ]
 
 if settings.DEBUG:
---
+++ customers_app/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+
+from .views import CustomersListView, CustomersCreateView, CustomersAuthView, CustomersDetailView,\
+                   export_customers_details_in_xlsx, CustomersVotingView, CustomersVotingAPIView
+
+urlpatterns = [
+    path('auth/', CustomersAuthView.as_view(), name='customers-auth'),
+    path('create/', CustomersCreateView.as_view(), name='customers-create'),
+    path('export/', export_customers_details_in_xlsx, name='customers-export'),
+    path('list/', CustomersListView.as_view(), name='customers-list'),
+    path('voting/', CustomersVotingView.as_view(), name='customers-voting'),
+    path('<int:pk>/', CustomersDetailView.as_view(), name='customers-detail'),
+    path('api/voting/', CustomersVotingAPIView.as_view(), name='api-customers-voting')
+]
